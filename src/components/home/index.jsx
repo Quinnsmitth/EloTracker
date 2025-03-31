1dda60b3ab213159e6ed31556e2bc8b9d7d763a6
jackr <jreppert@ithaca.edu>
2025-03-31 12:00:48 -0400

    imporved header and style

--- src/components/home/index.jsx
+++ src/components/home/index.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import { useAuth } from '../../authContext/index.jsx'
+import './Home.css'
 
 const Home = () => {
-    const { currentUser } = useAuth()
-    return (
-        <div className='text-2xl font-bold pt-14'>Hello {currentUser.displayName ? currentUser.displayName : currentUser.email}, you are now logged in.</div>
-    )
+  const { currentUser } = useAuth()
+  return (
+    <div className="home-container">
+      Hello {currentUser.displayName ? currentUser.displayName : currentUser.email}, you are now logged in.
+    </div>
+  )
 }
 
 export default Home
