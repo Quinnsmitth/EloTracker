4a2cf54f9bccb7ba3efb35bbb8c27018eddd72ba
jackr <jreppert@ithaca.edu>
2025-03-31 22:24:33 -0400

    added game rectangles and stylized nav bar

--- src/components/home/index.jsx
+++ src/components/home/index.jsx
@@ -6,7 +6,14 @@ const Home = () => {
   const { currentUser } = useAuth()
   return (
     <div className="home-container">
-      Hello {currentUser.displayName ? currentUser.displayName : currentUser.email}, you are now logged in.
+      <p>
+        Hello {currentUser.displayName ? currentUser.displayName : currentUser.email}, you are now logged in.
+      </p>
+      <div className="game-grid">
+        <div className="game-card">Rock Paper Scissors</div>
+        <div className="game-card">Chess</div>
+        <div className="game-card">Random Number Guesser</div>
+      </div>
     </div>
   )
 }
