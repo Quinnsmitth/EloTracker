6b3b42c4bc55ada793d06d119ea8aa083804502b
jackr <jreppert@ithaca.edu>
2025-04-09 14:56:44 -0400

    typo

--- src/components/profile/index.jsx
+++ src/components/profile/index.jsx
@@ -8,10 +8,10 @@ const Profile = () => {
     return (
         <div className="profile-container">
             <h2>Profile</h2>
-            <p>Email: {currentUser.displayName}</p>
+            <h3>{currentUser.displayName}</h3>
             <p>Chess Elo: 1500</p>
             <p>Random Number Guesser Elo: 1500</p>
-            <p>Rock Papaer Scissors Elo: 1500</p>
+            <p>Rock Paper Scissors Elo: 1500</p>
         </div>
     )
 }
