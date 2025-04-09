391c2bbac33bceb00144cfba48578a246ae6e0db
quinnsmith <qsmith@ithaca.edu>
2025-04-09 18:45:38 -0400

    generic chess input done

--- src/components/eloInput/index.jsx
+++ src/components/eloInput/index.jsx
@@ -3,7 +3,7 @@ import { useAuth } from '../../authContext'
 import { doc, updateDoc, getDoc } from 'firebase/firestore'
 import { firestore } from '../../firebase/firebase'
 //import './EloUpdater.css'
-
+//input elo
 const EloInput = () => {
     const { currentUser } = useAuth()
     const [opponentElo, setOpponentElo] = useState('')
